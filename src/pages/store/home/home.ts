55f2a9e6adb6d544d2b93d9bae5e4ccf361a4135
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2026-04-17 00:46:04 -0300

    busqueda - no funciona todavia

--- src/pages/store/home/home.ts
+++ src/pages/store/home/home.ts
@@ -22,11 +22,11 @@ const cargarProductos = () => {
     const productsContainer = document.getElementById("products-container") as HTMLDivElement;
 
     const products = getProducts();
-    
+
     products.forEach((PRODUCTS) => {
-        const productsCard : HTMLElement = document.createElement("div");
+        const productsCard: HTMLElement = document.createElement("div");
         productsCard.classList.add("featured-products");
-        productsCard.innerHTML =  `
+        productsCard.innerHTML = `
         <img src="/images/${PRODUCTS.imagen}" alt="Imagen de ${PRODUCTS.nombre}" />
         <p class=product-category>${PRODUCTS.categorias.map(c => c.nombre)}</p>
         <h3 class=product-name>${PRODUCTS.nombre}</h3>
@@ -38,9 +38,24 @@ const cargarProductos = () => {
         `
         productsContainer.appendChild(productsCard);
     });
-    
+
 }
 
 cargarProductos();
+
+// REVISAR!
+const searchProduct = document.getElementById("searchProduct") as HTMLInputElement;
+
+searchProduct.addEventListener("input", (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    const search = target.value.toLowerCase();
+
+    const searchResult = getProducts().filter((p) =>
+        p.nombre.toLowerCase().includes(search) ||
+        p.descripcion.toLowerCase().includes(search) ||
+        p.categorias.some(c => c.nombre.toLowerCase().includes(search))
+    );
+});
+
 
 
