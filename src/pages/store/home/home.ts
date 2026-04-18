7ed0597edf6eb60156c966b82f2976d0fe97f484
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2026-04-18 15:29:22 -0300

    funciona busqueda y filtro por categorias

--- src/pages/store/home/home.ts
+++ src/pages/store/home/home.ts
@@ -2,6 +2,13 @@ import type { Product } from "../../../types/product";
 import type { ICategory } from "../../../types/category";
 import { getCategories, getProducts, PRODUCTS } from "../../../data/data";
 
+const products = getProducts();
+const categories = getCategories();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadProducts(products);
+});
 
 const cargarCategorias = () => {
     const categoriesList = document.getElementById("categories-list") as HTMLUListElement;
@@ -13,49 +20,78 @@ const cargarCategorias = () => {
         li.classList.add("categories");
         categoriesList.appendChild(li);
     })
-
 };
 
 cargarCategorias();
 
-const cargarProductos = () => {
+//carga las tarjetas de productos
+const loadProducts = (products: Product[]) => {
     const productsContainer = document.getElementById("products-container") as HTMLDivElement;
+    productsContainer.innerHTML = "";
 
-    const products = getProducts();
-
-    products.forEach((PRODUCTS) => {
+    products.forEach((products) => {
         const productsCard: HTMLElement = document.createElement("div");
         productsCard.classList.add("featured-products");
         productsCard.innerHTML = `
-        <img src="/images/${PRODUCTS.imagen}" alt="Imagen de ${PRODUCTS.nombre}" />
-        <p class=product-category>${PRODUCTS.categorias.map(c => c.nombre)}</p>
-        <h3 class=product-name>${PRODUCTS.nombre}</h3>
-        <p class=product-description>${PRODUCTS.descripcion}</p>
-        <p class=product-price>Precio: $${PRODUCTS.precio}</p>
+        <div class="featured-img">
+        <img src="/images/${products.imagen}" alt="Imagen de ${products.nombre}" /></div>
+        <p class=product-category>${products.categorias.map(c => c.nombre)}</p>
+        <h3 class=product-name>${products.nombre}</h3>
+        <p class=product-description>${products.descripcion}</p>
+        <p class=product-price>Precio: $${products.precio}</p>
         <div class="buttons">
-        <button class=btn-details data-id="${PRODUCTS.id}">Ver Detalles</button>
-        <button class=btn-cart data-id="${PRODUCTS.id}">Agregar al Carrito</button></div>
+        <button class=btn-details data-id="${products.id}">Ver Detalles</button>
+        <button class=btn-cart data-id="${products.id}">Agregar al Carrito</button></div>
         `
         productsContainer.appendChild(productsCard);
     });
 
 }
 
-cargarProductos();
+//busqueda por nombre 
+const inputSearch = document.getElementById("searchProduct") as HTMLInputElement
+const searchNotification = document.getElementById("searchNotification") as HTMLElement;
 
-// REVISAR!
-const searchProduct = document.getElementById("searchProduct") as HTMLInputElement;
-
-searchProduct.addEventListener("input", (e: Event) => {
+inputSearch.addEventListener("input", (e) => {
     const target = e.target as HTMLInputElement;
     const search = target.value.toLowerCase();
 
-    const searchResult = getProducts().filter((p) =>
-        p.nombre.toLowerCase().includes(search) ||
-        p.descripcion.toLowerCase().includes(search) ||
-        p.categorias.some(c => c.nombre.toLowerCase().includes(search))
-    );
+    const searchResults = products.filter((product) => {
+        return product.nombre.toLowerCase().includes(search);
+    });
+    loadProducts(searchResults);
+
+    //Muestra u oculta el contador de producos encontrados
+    if (search === "") {
+        searchNotification.style.display = "none";
+    } else if (searchResults.length > 0) {
+        searchNotification.style.display = "block";
+        searchNotification.textContent = `Se encontraron ${searchResults.length} productos`;
+    } else {
+        searchNotification.textContent = "No hay productos con ese nombre";
+    }
+
 });
 
 
+//Filtrar por categorias
+const btnCategories = document.querySelectorAll<HTMLButtonElement>(".categories");
 
+btnCategories.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const selectedCategory = btn.textContent?.trim();
+
+        //busca categoria por nombre
+        const findCategory = categories.find(
+            (category) => category.nombre.toLowerCase() === selectedCategory?.toLowerCase()
+        );
+
+        console.log("Categoria clickeada", findCategory)
+        // Filtrar los prodcutos por categoria
+        const filterProduct = products.filter((product) =>
+            product.categorias.some((c) => c.id === findCategory?.id)
+        );
+
+        loadProducts(filterProduct);
+    });
+});
